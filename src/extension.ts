80c4f6177f4ec3bf203fbbefa9dda3cbbf0150e5
skewballfox <joshua.ferguson.273@gmail.com>
2021-05-19 10:14:27 -0500

    MINIMAL RUNNING STARTING POINT

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,28 @@
-//enable eslint
+//enable eslint;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as util from 'util';
+import { homedir } from 'os';
 //declare module "../../../sniper-node";
-import { Sniper } from '../../../sniper-node';
+const sniper = require('sniper-node');
+
 import { strict } from 'assert';
 //TODO: figure out how to start sniper on any editing session, not just onLanguage
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-console.log(process.version);
-const snipe = new Sniper("../../../sniper-core/snippets/", "python");
+//console.log(process.version);
+//TODO: create 'startSniper' function to either attach to running instance
+//or create new instance
+
 
 
 export function activate(context: vscode.ExtensionContext) {
-
+	let fileName = vscode.window.activeTextEditor?.document.fileName;
+	let language = vscode.window.activeTextEditor?.document.languageId;
+	if (fileName && language) {
+		sniper.add_target(vscode.env.sessionId, fileName, language);
+	}
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log("sniper started");
@@ -23,30 +31,14 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sniper.testit', async () => {
+	let disposable = vscode.commands.registerCommand('sniper.testit', () => {
 		// The code you place here will be executed every time your command is executed
-		let activeEditor = vscode.window.activeTextEditor;
-		if (activeEditor) {
-			const definitions = await vscode.commands.executeCommand<vscode.Location[]>(
-				'vscode.executeDefinitionProvider',
-				activeEditor.document.uri,
-				activeEditor.selection.active
-			);
-			if (definitions) {
-				for (const definition of definitions) {
-					console.log(definition.toString());
-				}
-			}
-		}
+
 		//output();
 
 		// Display a message box to the user
-		let activeDocument = activeEditor?.document;
-		if (!activeDocument) {
-			return;
-		}
 
-		vscode.window.showInformationMessage(activeDocument.fileName);
+		vscode.window.showInformationMessage("hello from sniper");
 	});
 
 	context.subscriptions.push(disposable);
